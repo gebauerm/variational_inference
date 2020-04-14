6d6075c03fe7edebf4a6410bc4186473f6867b4d
elysias <gebauerm23@gmail.com>
2020-04-14 16:04:03 +0200

    set up starting point

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='variational_inference',
+    version='0.0.1',
+    packages=['src'],
+    url='',
+    license='',
+    author='michael',
+    author_email='gebauerm23@gmail.com',
+    description=''
+)
