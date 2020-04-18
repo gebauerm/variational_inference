a6a6fadd4bb6872cd826a44d715337d404fdfebb
elysias <gebauerm23@gmail.com>
2020-04-18 19:54:25 +0200

    adds multinomial values for variances

--- scripts/variational_inference_start.py
+++ scripts/variational_inference_start.py
@@ -13,13 +13,14 @@ from scripts.utils import plot_elbo
 
 # ===================== Distribution P ===================
 # hyperparameters of P(mu)
-mu_mean = 0
-sigma = 1 # this variance is known
 k = 4
+mu_mean = 0
+sigma = np.array([1]*k) # this variance is known
+
 
 # number of instances
-i = 2000
-c_probas = [1/k]*k
+i = 200
+c_probas = np.array([1/k]*k)
 
 # construct probability model
 mu = np.random.normal(mu_mean, sigma, k)
@@ -33,10 +34,10 @@ iterations = 200
 # ================== Initialize Variational Parameters =================
 # Hypterparameter for Q(mu| m_q, s_q)
 mu_est = np.random.normal(0, 1, k)
-sigma_est = np.random.normal(0, 1, 1)
+sigma_est = np.random.normal(0, 1, k)
 
 m_q = np.ones(k)
-s_q=1
+s_q = np.ones(k)
 
 
 # Hyperparameter for Q(c)
@@ -53,21 +54,21 @@ for iter in range(iterations):
     for idx, x_i in enumerate(x):
         phi_row = np.ones(k)
         for idk in range(k):
-            phi_row[idk] = math.exp(mu_est[idk]*x_i - (mu_est[idk]**2 + sigma_est)/2)
+            phi_row[idk] = math.exp(mu_est[idk]*x_i - (mu_est[idk]**2 + sigma_est[idk])/2)
         phi_q[idx] = phi_row/phi_row.sum()
 
     for idk in range(k):
         # calculate m_q for every k
         sum_nominator = sum([phi_q[idx][idk]*x[idx] for idx, x_idx in enumerate(x)]) # check if convergence doesnt work
         sum_denominator = sum([phi_q[idx][idk] for idx, x in enumerate(x)])
-        m_q[idk] = sum_nominator/(1/sigma + sum_denominator)
+        m_q[idk] = sum_nominator/(1/sigma[idk] + sum_denominator)
 
         # calculate s_q for every k
-        s_q = 1/(1/sigma + sum_denominator)
+        s_q[idk] = 1/(1/sigma[idk] + sum_denominator)
 
     # ==== compute elbo
     # First Component sum_k(E[log p(mu_k)| m_k, s_k])
-    e_p_mu_k = sum([-mu_est[idk]**2 - sigma_est for idk in range(k)])
+    e_p_mu_k = sum([-mu_est[idk]**2 - sigma_est[idk] for idk in range(k)])
     # Second Component e_log_p(ci)
     e_log_p_c = -i*math.log(k)
     # Third Component e_log_p(xi|ci_mu)
@@ -76,7 +77,7 @@ for iter in range(iterations):
     # Forth Component e_log_q(c_i | phi_i)
     e_log_p_ci = sum([sum([phi_q[idx][idk]*math.log(phi_q[idx][idk]) for idk in range(k)]) for idx in range(i)])
     # Fith Component e_log_q_mu_k|m_k,s_k)
-    e_log_mu_k_q = sum([(math.log(2*math.pi) + sigma_est)/2 for idk in range(k)]) # here it might be one according to princton
+    e_log_mu_k_q = sum([(math.log(2*math.pi) + sigma_est[idk])/2 for idk in range(k)]) # here it might be one according to princton
     # compute elbo
     elbo = e_p_mu_k + e_log_p_c+e_log_x - e_log_p_ci -e_log_mu_k_q
     elbo_vals.append(elbo)
