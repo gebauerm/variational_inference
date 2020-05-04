5485d2008710808853af398ab6c9b6a2ec059409
elysias <gebauerm23@gmail.com>
2020-05-04 20:10:35 +0200

    added plots for working script

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,10 +1,30 @@
 from typing import List
 import plotly.express as px
 import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+import numpy as np
+from scipy import stats
 
 def plot_elbo(values: List):
     values = pd.DataFrame({
         "Iteration": list(range(len(values))),
         "ELBO":values})
     fig = px.line(values, x="Iteration", y="ELBO", title='Variational Inference Convergence')
+    return fig
+
+
+def plot_distributions(x, targets, mu_values=None):
+    plot_dict = {"x" : x,
+                 "distribution": targets
+                 }
+    plot_df = pd.DataFrame.from_dict(plot_dict)
+    val_range = np.arange(int(round(plot_df["x"].min())), int(round(plot_df["x"].max()+1)), 0.2)
+    fig = px.histogram(plot_df, x="x", color="distribution", nbins=100, opacity=0.7, histnorm='probability density')
+    traces = [go.Scatter(x = val_range,
+                         y = stats.norm(loc=mu_values[idx], scale=1).pdf(val_range),
+                         mode="lines",
+                         name=f"estimated_dist_{idx}") for idx, _ in enumerate(plot_df["distribution"].unique().tolist())]
+    for trace in traces:
+        fig.add_trace(trace)
     return fig
--- scripts/variational_inference_start.py
+++ scripts/variational_inference_start.py
@@ -1,10 +1,12 @@
 # Reference: https://arxiv.org/pdf/1601.00670.pdf
 # https://ssl2.cms.fu-berlin.de/ewi-psy/einrichtungen/arbeitsbereiche/computational_cogni_neurosc/PMFN/15-Variational-inference.pdf
+# https://zhiyzuo.github.io/VI/
 import numpy as np
 import math
-from math import log2
-from scipy.stats import norm, uniform
-from scripts.utils import plot_elbo
+from scripts.utils import *
+import pandas as pd
+
+import seaborn as sns
 
 # TODO: find errors in the ELBO
 # TODO: fix the variance estimation issue
@@ -19,14 +21,15 @@ sigma = np.array([1]*k) # this variance is known
 
 
 # number of instances
-i = 200
+i = 2000
 c_probas = np.array([1/k]*k)
 
 # construct probability model
-mu = np.random.normal(mu_mean, sigma, k)
+mu = np.random.normal(mu_mean, 10, k)
 c = np.random.multinomial(n=1, pvals=c_probas, size= i)
+distribution_labels = [f"dist_{np.where(c[row, :] == 1)[0][0]}" for row in range(c.shape[0])]
 x_mu = c.dot(mu)
-x = np.random.normal(x_mu, 1)
+x = np.array([np.random.normal(mu_value, 1, 1)[0] for mu_id, mu_value in enumerate(x_mu)])
 
 # optimization params
 iterations = 200
@@ -34,7 +37,7 @@ iterations = 200
 # ================== Initialize Variational Parameters =================
 # Hypterparameter for Q(mu| m_q, s_q)
 mu_est = np.random.normal(0, 1, k)
-sigma_est = np.random.normal(0, 1, k)
+sigma_est = np.random.normal(0, 1, k)**2
 
 m_q = np.ones(k)
 s_q = np.ones(k)
@@ -68,16 +71,16 @@ for iter in range(iterations):
 
     # ==== compute elbo
     # First Component sum_k(E[log p(mu_k)| m_k, s_k])
-    e_p_mu_k = sum([-mu_est[idk]**2 - sigma_est[idk] for idk in range(k)])
+    e_p_mu_k = sum([mu_est[idk]**2/sigma[idk] for idk in range(k)])
     # Second Component e_log_p(ci)
     e_log_p_c = -i*math.log(k)
     # Third Component e_log_p(xi|ci_mu)
-    e_log_x = sum([sum([phi_q[idx][idk]*x[idx]*mu_est[idk] - phi_q[idx][idk]*mu_est[idk]**2/2 for idx, _ in enumerate(x)])
+    e_log_x = sum([sum([-phi_q[idx][idk]*x[idx]*mu_est[idk] + phi_q[idx][idk]*mu_est[idk]**2/2 for idx, _ in enumerate(x)])
                    for idk in range(k)])
     # Forth Component e_log_q(c_i | phi_i)
     e_log_p_ci = sum([sum([phi_q[idx][idk]*math.log(phi_q[idx][idk]) for idk in range(k)]) for idx in range(i)])
     # Fith Component e_log_q_mu_k|m_k,s_k)
-    e_log_mu_k_q = sum([(math.log(2*math.pi) + sigma_est[idk])/2 for idk in range(k)]) # here it might be one according to princton
+    e_log_mu_k_q = sum([math.log(sigma_est[idk])/2 for idk in range(k)]) # here it might be one according to princton
     # compute elbo
     elbo = e_p_mu_k + e_log_p_c+e_log_x - e_log_p_ci -e_log_mu_k_q
     elbo_vals.append(elbo)
@@ -97,3 +100,5 @@ print(f"Actual Mean Vals: {mu}")
 print(f"Estimated Sigma: {sigma_est}")
 print(f"Real Sigma: {sigma}")
 
+fig_2 = plot_distributions(x, targets=distribution_labels, mu_values=mu_est)
+fig_2.show()
