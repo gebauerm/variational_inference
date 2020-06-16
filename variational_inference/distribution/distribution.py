77572621f55c63952cb9e5a14d78e3ee1dd9c768
Michael Gebauer <michael.y.gebauer@pwc.com>
2020-06-16 09:30:14 -0700

    added interface idea

--- variational_inference/distribution/distribution.py
+++ variational_inference/distribution/distribution.py
@@ -1,1 +1,10 @@
+import abc
+
 # TODO: define classes for every distribution needed so far to store parameters and random data generation functionality
+
+
+class Distribution(abc.ABC):
+
+    @abc.abstractmethod
+    def sample(self):
+        pass
